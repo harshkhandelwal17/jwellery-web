c187751fdbeed91a9be03f63e46c95b334ea531a
harshkhandelwal17 <152009370+harshkhandelwal17@users.noreply.github.com>
2026-05-12 01:02:28 +0530

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "picsum.photos",
       },
-      {
+      { 
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
